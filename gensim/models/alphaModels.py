2f21e2cef33baa9e89a4c6cfa882556324091f3a
octavius-1993 <octavius-1993@users.noreply.github.com>
2015-09-09 17:21:09 +0100

    Updated code for alphaModels.py

--- gensim/models/alphaModels.py
+++ gensim/models/alphaModels.py
@@ -2,19 +2,31 @@
 #
 # Allow support for alpha variants in LDA models
 
+"""
+Alpha objects facilitate the handling of alpha values in the mLDA model.
+
+In mLDA, each document is allowed to have its own alpha value.
+In most applications, only one or two distinct values are required.
+The Alpha objects allow alpha values to be specified without bloating the data.
+"""
+
+
+
+
 import numpy
 
 class Alpha(object):
-    """Alpha pretends to be a K by D array.
-    Calling __getitem__ on an Alpha should produce
+    """Alpha acts like a K by D array.
+    Calling ``__getitem__`` on an Alpha object should produce
     a K-dimensional vector.
     In the basic class, a K by D array is required."""
     
     def __init__(self, nparray):
-        """Provide a K by D array with the dth alpha in column d"""
+        """Provide a K by D numpy array."""
         self.array = nparray
 
     def __getitem__(self, documentId):
+        """Returns a K-dimensional vector, or a new Alpha object if sliced."""
         if isinstance(documentId, slice):
             return Alpha(self.array[:, documentId])
         else:
@@ -24,7 +36,7 @@ class Alpha(object):
 
 
 class UniformAlpha(Alpha):
-    """A uniform alpha returns the same vector
+    """A uniform alpha returns the same alpha vector
     for every document"""
     
     def __init__(self, nparray):
@@ -36,10 +48,25 @@ class UniformAlpha(Alpha):
             return self
         else:
             return self.array
+        
+        
+class UniformSymmetricAlpha(UniformAlpha):
+    """A uniform symmetric alpha returns the same symmetric alpha for every document. The symmetric alpha is
+    ``[alpha]*K``
+    """
+    def __init__(self, alpha, length):
+        """Provide a (floating point) alpha value"""
+        self.array = numpy.asarray([alpha]*length)
+        self.alpha=alpha
+    
+    def __str__(self):
+        return "Uniform symmetric alpha = %f" % self.alpha
     
 class TwoPartAlpha(Alpha):
-    """A two part alpha returns alpha_1 for documents 1, ..., D_1
-    and alpha_2 for D_1 + 1, ..., D_1 + D_2"""
+    """A two part alpha returns:
+    - alpha_1 for documents 1, ..., D_1
+    - alpha_2 for D_1 + 1, D_1 + 2, ...
+    """
     
     def __init__(self, array1, array2, D1):
         """Provide the arrays for each subcorpus and the length of
@@ -57,7 +84,10 @@ class TwoPartAlpha(Alpha):
             return self.array2
         
 class TwoPartSymmetricAlpha(TwoPartAlpha):
-    """Provides a symmetric version of two part alpha"""
+    """A two part symmetric alpha returns:
+    - symmetric vector of alpha_1 for documents 1, ..., D_1
+    - symmetric vector of alpha_2 for D_1 + 1, D_1 + 2, ...
+    """
     
     def __init__(self, alpha1, alpha2, D1, K):
         """alpha1 and alpha2 are alpha values to be used
@@ -69,27 +99,12 @@ class TwoPartSymmetricAlpha(TwoPartAlpha):
         self.array1 = numpy.asarray([alpha1]*K)
         self.array2 = numpy.asarray([alpha2]*K)
         self.cutoff = D1
+        self.alpha1 = alpha1
+        self.alpha2 = alpha2
+        
+    def __str__(self):
+        return "Two part alpha split at %d, alpha1 = %f, alpha2 = %f" % (self.cutoff, self.alpha1, self.alpha2)    
         
         
-if __name__ == '__main__':
-    a = numpy.asarray([[1,2,3],[4,5,6]])
-    alpha = Alpha(a)
-    print(alpha[0])
-    uniAlpha = UniformAlpha(a[0, :])
-    print(uniAlpha[100])
-    twoAlpha = TwoPartAlpha(a[0,:], a[1,:], 12)
-    print(twoAlpha[0])
-    print(twoAlpha[12])
-    print(twoAlpha[11])
-    
-    print("Now test slice")
-    print((alpha[0:2]).array)
-    print(uniAlpha[0:100][40])
-    print(twoAlpha[5:50][6])
-    print(twoAlpha[5:50][7])
-    
-    print("Test symmetric two part")
-    alpha2sym = TwoPartSymmetricAlpha(6, 7, 12, 10)
-    print(alpha2sym[11])
-    print(alpha2sym[12])
+
     
